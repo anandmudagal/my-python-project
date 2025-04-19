b1b1f4414782b10462c5b43e4bb5a5ce53e865e8
anandmudagal <40746636+anandmudagal@users.noreply.github.com>
2025-04-19 11:42:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from utils import greet
 if __name__ == "__main__":
     greet("World")
+    greet("World")
+    greet("World")
+    greet("World")
+    greet("World")
     # This is a simple Python script that imports a function from another module and calls it.
